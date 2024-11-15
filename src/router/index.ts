9b84a271cb287aeebba3330d1ce70d36c3604aee
reaget <reaget.acc@gmail.com>
2024-11-15 21:51:08 +0300

    build: update base url

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,6 +43,6 @@ const routes: RouteRecordRaw[] = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.VITE_BASE_PATH),
   routes,
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// TODO: add env variables to public vars
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/sarafan-frontend/',
   build: {
     outDir: './docs'
   },
